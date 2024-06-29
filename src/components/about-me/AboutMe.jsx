24b583a167c04d9d8c367a191e5f01b8c8befc4d
Ilhom <ilhom@example.com>
2024-06-29 11:43:21 +0500

    homework

--- src/components/about-me/AboutMe.jsx
+++ src/components/about-me/AboutMe.jsx
@@ -1,12 +1,30 @@
 import "./AboutMe.css"
 import Footer from "../footer/Footer.jsx";
 import Nav from "../navbar/Nav.jsx";
+import userImg from '../../images/About Me.png'
+
 
 const AboutMe = () => {
     return (
         <div className="about-me">
             <Nav/>
-
+            <div className="box">
+                <div className="image">
+                    <img src={userImg} alt="User image"/>
+                </div>
+                <div className="info">
+                    <h5>About</h5>
+                    <h2>About Me</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet eaque incidunt molestias odio odit
+                        sapiente sit, veniam. Aliquid autem blanditiis consequuntur cumque dignissimos doloribus eius,
+                        ex fugit magnam magni nulla praesentium quis quisquam quos ullam velit vitae voluptates
+                        voluptatum!
+                    </p>
+                    <br/>
+                    <br/>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus enim fugit sapiente sed vel!</p>
+                </div>
+            </div>
             <Footer/>
         </div>
     )
